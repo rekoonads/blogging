4ceb5ea2b9e413426ce8c6f7a2be5a4ad39308bd
ayan471 <pakhiraayan@gmail.com>
2024-11-11 18:58:43 +0530

    creating side menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,45 @@
 import React from "react";
-import styles from "./menu.modules.css";
+import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What&apos;s hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categoryList}>
+        <Link
+          href={"/blog?cat=style"}
+          className={`${styles.categoryItem} ${styles.style}`}
+        >
+          Styles
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.fashion}`}>
+          fashion
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.food}`}>
+          food
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.travel}`}>
+          Travel
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.culture}`}>
+          Culture
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.coding}`}>
+          coding
+        </Link>
+      </div>
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
+    </div>
+  );
 };
 
 export default Menu;
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 
 export default function AuthLinks() {
+  const [open, setOpen] = useState(false);
+
   const status = "notauthenticated";
 
   return (
     <>
       {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write" className="text-primary hover:underline mr-4">
+          <Link href="/write" className={styles.link}>
             Write
           </Link>
           <button className={styles.link}>Logout</button>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
