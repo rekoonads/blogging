416e3fde18f5e1ea2a3be51932b7e8c805fb6e03
ayan471 <pakhiraayan@gmail.com>
2024-11-13 15:13:35 +0530

    updated url

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -23,7 +23,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `https://blogging-pink.vercel.app/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
